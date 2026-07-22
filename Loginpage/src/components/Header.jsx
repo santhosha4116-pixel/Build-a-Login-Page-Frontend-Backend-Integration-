561bda9cec63e5321dd54267481a97f977ab2e39
santhosha4116-pixel <santhosha4116@gmail.com>
2026-07-22 18:33:59 +0530

    delete all

--- Loginpage/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header(props){
-    return(
-        <div className="mb-2">
-            <h1 className='text-3xl font-bold'>Welcome Santhosh</h1>
-         <p>I will help you to manage activity :)</p> 
-        </div>
-    )
-}
-export default Header
--- Loginpage/src/components/card.jsx
+++
@@ -1,9 +0,0 @@
-function Card(props){
-    return(
-          <div  style={{backgroundColor:props.bgcolor}}className=' py-3 px-6  rounded-md  grow  text-center '>
-            <h1 className= 'text-3xl font-bold'>{props.title}</h1>
-            <p className='font-normal text-2xl'>{props.descr}</p>
-          </div>
-    )
-}
-export default Card
