9290b700febb8f4f9be8419821ab0072a0b1d474
santhosha4116-pixel <santhosha4116@gmail.com>
2026-07-22 19:20:47 +0530

    fouth commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Route,Router,BrowserRouter, Routes } from 'react-router-dom'
 import './index.css'
 import Loginpage from './Loginpage.jsx'
-import App from './App.jsx'
 import Landing from './landingpage.jsx'
-
 createRoot(document.getElementById('root')).render(
   
   <BrowserRouter>
